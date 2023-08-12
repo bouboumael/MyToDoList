c622c376758d076bc4ceb6617e7bda8f9e159f8d
maelchariault <bouboumael@gmail.com>
2023-08-13 00:31:35 +0200

    save

---
+++ MyToDoList/src/view/components/TaskView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskView.swift
+//  MyToDoList
+//
+//  Created by Mael Chariault on 12/08/2023.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskView()
+    }
+}
